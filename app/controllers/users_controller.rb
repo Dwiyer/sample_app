32eec9dea70f4a7fdfa93d76ec7a9f5561206079
Dwiyer <dwiyer@gmail.com>
2013-04-17 21:03:35 -0400

    Modify update-delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users=User.paginate(page: params[:page])
+    @users=User.paginate(page: params[:page], per_page: 10)
   end
   
   def edit
